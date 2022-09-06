b8275fe30f985c72a54a3534b9824ee691cd3847
Christian Beckham <urenacb22@gmail.com>
2022-09-06 17:29:32 -0500

    feat: created player_one and player_two instance variables, and display_welcome method

--- game.py
+++ game.py
@@ -1,6 +1,13 @@
+from player import Player
+
+
 class Game:
     def __init__(self):
-        pass
+        self.player_one = Player('Player 1')
+        self.player_two = Player('Player 2')
 
     def start_game(self):
-        pass
+        self.display_welcome()
+
+    def display_welcome(self):
+        print('\nWelcome to Battleship!')
