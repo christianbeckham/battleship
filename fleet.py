7d703650bcfc797570d43b0f9af89e95fc43a6bd
Christian Beckham <urenacb22@gmail.com>
2022-09-06 16:57:01 -0500

    feat: updated fleet class with child ship imports and initialize child ships in the ship instance variable

--- fleet.py
+++ fleet.py
@@ -1,3 +1,11 @@
+from carrier import Carrier
+from battleship import Battleship
+from cruiser import Cruiser
+from submarine import Submarine
+from destroyer import Destroyer
+
+
 class Fleet:
     def __init__(self):
-        self.ships = []
+        self.ships = [Carrier(), Battleship(), Cruiser(),
+                      Submarine(), Destroyer()]
